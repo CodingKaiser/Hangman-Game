3634da927d63746f370e9e518c512d35891813b7
CodingKaiser <falkonoe@live.com>
2017-02-09 22:29:21 -0800

    Finished some initializing js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,36 @@
-var count = 0;
+var guesses = 8;
+var words = ['this', 'is', 'a', 'test', 'pernicious'];
 
-$(document).keypress(function(e) {
-	console.log("Key pressed");
-	count += 1;
-	$("#solution").text("" + count);
-});
+var game = {
+	currentWord: '',
+	incompleteWord: [],
+	guessesRemaining: 0,
+	wins: 0,
+	guessedLetters: [],
+
+	start: function () {
+		var newWord = words[Math.floor(Math.random() * words.length)];
+		this.currentWord = newWord;
+		this._setIncompleteWord();
+	},
+
+	_setIncompleteWord: function () {
+		for (var i = 0; i < this.currentWord.length; i++) {
+			this.incompleteWord += ["_"];
+		}
+		console.log(this.incompleteWord);
+	},
+
+	parseInput: function (character) {
+		console.log("Pressed: " + character);
+		document.getElementById("solution").textContent = keyPressed;
+	}
+}
+
+document.onkeyup = function(event) {
+	var keyPressed = event.key;
+	console.log(keyPressed);
+	game.test(keyPressed);
+};
+
+game.start();
