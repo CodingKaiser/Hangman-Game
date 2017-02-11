d4c82c80e43ddcd0d448f5228c57c09dfda3a09e
CodingKaiser <falkonoe@live.com>
2017-02-10 22:46:42 -0800

    Basics of game logic are working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,83 @@
-var guesses = 8;
 var words = ['this', 'is', 'a', 'test', 'pernicious'];
 
 var game = {
+	maxGuesses: 0,
 	currentWord: '',
 	incompleteWord: [],
-	guessesRemaining: 0,
+	guesses: 0,
 	wins: 0,
-	guessedLetters: [],
+	guessedLetters: new Set(),
+	guessedLettersDisplay: [],
 
 	start: function () {
+		this._resetValues();
 		var newWord = words[Math.floor(Math.random() * words.length)];
 		this.currentWord = newWord;
+		this.maxGuesses = newWord.length * 1.5;
 		this._setIncompleteWord();
 	},
 
+	_resetValues: function() {
+		this.guesses = 0;
+		this.guessedLetters = new Set();
+		this.guessedLettersDisplay = [];
+		this.incompleteWord = [];
+	},
+
 	_setIncompleteWord: function () {
+		var textToDisplay = "";
 		for (var i = 0; i < this.currentWord.length; i++) {
-			this.incompleteWord += ["_"];
+			textToDisplay += "_  ";
+			this.incompleteWord.push("_");
 		}
-		console.log(this.incompleteWord);
+		document.getElementById("word-thus-far").textContent = textToDisplay;
+	},
+
+	_updateIncompleteWord: function () {
+		var textToDisplay = "";
+		for (var i = 0; i < this.currentWord.length; i++) {
+			textToDisplay += this.incompleteWord[i] + " ";
+		}
+		document.getElementById("word-thus-far").textContent = textToDisplay;
 	},
 
 	parseInput: function (character) {
 		console.log("Pressed: " + character);
-		document.getElementById("solution").textContent = keyPressed;
+		console.log("Already pressed key? " + (this.guessedLetters.has(character)));
+		document.getElementById("solution").textContent = character;
+		if (!(this.guessedLetters.has(character))) {
+			if (this.currentWord.includes(character)) {
+				indexesOfTypedCharacter = [];
+				for (var i = 0; i < this.currentWord.length; i++) {
+					if (this.currentWord[i] === character) {
+						indexesOfTypedCharacter.push(i);
+					}
+				}
+				for (var j = 0; j < indexesOfTypedCharacter.length; j++) {
+					this.incompleteWord[indexesOfTypedCharacter[j]] = character;
+				}
+			} else {
+				this.guessedLettersDisplay.push(character);
+			}
+			this.guessedLetters.add(character);
+			this._updateIncompleteWord();
+			this.guesses += 1;
+		}
+		if (this.guesses >= this.maxGuesses) {
+			this._resetValues();
+			this.start();
+		} else if (!(this.incompleteWord.includes("_"))) {
+			this._resetValues();
+			this.wins += 1;
+			document.getElementById("win-number").textContent = this.wins;
+			this.start();
+		}
 	}
 }
 
 document.onkeyup = function(event) {
 	var keyPressed = event.key;
-	console.log(keyPressed);
-	game.test(keyPressed);
+	game.parseInput(keyPressed);
 };
 
 game.start();
