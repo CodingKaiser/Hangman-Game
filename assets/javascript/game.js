03e2d37121f5843ad81045c50cb0adb23dc10bf8
CodingKaiser <falkonoe@live.com>
2017-02-12 15:05:12 -0800

    Reformated code and added functionality to switch videos.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,17 @@
-var words = ['this', 'is', 'a', 'test', 'pernicious'];
+var words = [['parking', 'https://www.youtube.com/embed/lkq-Fd9TU8k'], 
+			['jolly', 'https://www.youtube.com/embed/TQSMmwQyEjk'], 
+			['schroedinger', 'https://www.youtube.com/embed/6HJqPZ-KmZs'], 
+			['flagpole', 'https://www.youtube.com/embed/b8U1na74Bcc'], 
+			['marijuana', 'https://www.youtube.com/embed/SAPKjWHikzM'], 
+			['abraxas', 'https://www.youtube.com/embed/3tfI9tTzlI0'], 
+			['breakdown', 'https://www.youtube.com/embed/uvIZ4ST6HqE'], 
+			['gambling', 'https://www.youtube.com/embed/uyj1CeZt23A'], 
+			['marshak', 'https://www.youtube.com/embed/aQQsBjOrNMY']];
+var allowedChars = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
 
 var game = {
 	maxGuesses: 0,
+	currentWordIndex: -1,
 	currentWord: '',
 	incompleteWord: [],
 	guesses: 0,
@@ -11,10 +21,53 @@ var game = {
 
 	start: function () {
 		this._resetValues();
-		var newWord = words[Math.floor(Math.random() * words.length)];
-		this.currentWord = newWord;
-		this.maxGuesses = newWord.length * 1.5;
+		this._updateCurrentWordIndex();
+		this._setNewWordAndUpdateVideo();
 		this._setIncompleteWord();
+		this._updateNumberOfGuessesRemainingHTML();
+		this._updateIncorrectGuessesHTML();
+		this._updateIncompleteWordHTML();
+	},
+
+
+	parseInput: function (character) {
+		console.log("Pressed: " + character);
+		if (this._isValidCharacter(character) && 
+			this._isUniqueCharacter(character)) {
+			console.log("Already pressed key? " + 
+				(this.guessedLetters.has(character)));
+			this._parseLetter(character);
+		}
+		if (this.guesses >= this.maxGuesses) {
+			this._resetValues();
+			this.start();
+		} else if (this._currentWordHasBeenGuessed()) {
+			this._resetValues();
+			this.wins += 1;
+			this._updateWinsHTML();
+			this.start();
+		}
+	},
+
+	_parseLetter(character) {
+		if (this.currentWord.includes(character)) {
+			indexesOfTypedCharacter = [];
+			for (var i = 0; i < this.currentWord.length; i++) {
+				if (this.currentWord[i] === character) {
+					indexesOfTypedCharacter.push(i);
+				}
+			}
+			for (var j = 0; j < indexesOfTypedCharacter.length; j++) {
+				this.incompleteWord[indexesOfTypedCharacter[j]] = character;
+			}
+		} else {
+			this.guessedLettersDisplay.push(character);
+			this._updateIncorrectGuessesHTML();
+		}
+		this.guessedLetters.add(character);
+		this._updateIncompleteWordHTML();
+		this._updateNumberOfGuessesRemainingHTML();
+		this.guesses += 1;
 	},
 
 	_resetValues: function() {
@@ -22,6 +75,22 @@ var game = {
 		this.guessedLetters = new Set();
 		this.guessedLettersDisplay = [];
 		this.incompleteWord = [];
+	},
+
+	_updateCurrentWordIndex() {
+		if (this.currentWordIndex < words.length - 1) {
+			this.currentWordIndex += 1;
+		} else {
+			this.currentWordIndex = 0;
+		}
+	},
+
+	_setNewWordAndUpdateVideo() {
+		var newWord = words[this.currentWordIndex][0];
+		var hintVideo = words[this.currentWordIndex][1];
+		this.currentWord = newWord;
+		this.maxGuesses = Math.round(newWord.length * 1.75);
+		document.getElementById("main-image").src = hintVideo;
 	},
 
 	_setIncompleteWord: function () {
@@ -33,7 +102,7 @@ var game = {
 		document.getElementById("word-thus-far").textContent = textToDisplay;
 	},
 
-	_updateIncompleteWord: function () {
+	_updateIncompleteWordHTML: function () {
 		var textToDisplay = "";
 		for (var i = 0; i < this.currentWord.length; i++) {
 			textToDisplay += this.incompleteWord[i] + " ";
@@ -41,37 +110,32 @@ var game = {
 		document.getElementById("word-thus-far").textContent = textToDisplay;
 	},
 
-	parseInput: function (character) {
-		console.log("Pressed: " + character);
-		console.log("Already pressed key? " + (this.guessedLetters.has(character)));
-		document.getElementById("solution").textContent = character;
-		if (!(this.guessedLetters.has(character))) {
-			if (this.currentWord.includes(character)) {
-				indexesOfTypedCharacter = [];
-				for (var i = 0; i < this.currentWord.length; i++) {
-					if (this.currentWord[i] === character) {
-						indexesOfTypedCharacter.push(i);
-					}
-				}
-				for (var j = 0; j < indexesOfTypedCharacter.length; j++) {
-					this.incompleteWord[indexesOfTypedCharacter[j]] = character;
-				}
-			} else {
-				this.guessedLettersDisplay.push(character);
-			}
-			this.guessedLetters.add(character);
-			this._updateIncompleteWord();
-			this.guesses += 1;
+	_updateIncorrectGuessesHTML: function () {
+		var textToDisplay = "";
+		for (var i = 0; i < this.guessedLettersDisplay.length; i++) {
+			textToDisplay += this.guessedLettersDisplay[i].toUpperCase() + " ";
 		}
-		if (this.guesses >= this.maxGuesses) {
-			this._resetValues();
-			this.start();
-		} else if (!(this.incompleteWord.includes("_"))) {
-			this._resetValues();
-			this.wins += 1;
-			document.getElementById("win-number").textContent = this.wins;
-			this.start();
-		}
+		document.getElementById("guess-letters-incorrect").textContent = textToDisplay;
+	},
+
+	_updateNumberOfGuessesRemainingHTML: function () {
+		document.getElementById("guess-number").textContent = this.maxGuesses - this.guesses - 1;
+	},
+
+	_updateWinsHTML() {
+		document.getElementById("win-number").textContent = this.wins;
+	},
+
+	_isValidCharacter(character) {
+		return allowedChars.has(character.toLowerCase());
+	},
+
+	_isUniqueCharacter(character) {
+		return !(this.guessedLetters.has(character));
+	},
+
+	_currentWordHasBeenGuessed() {
+		return !(this.incompleteWord.includes("_"));
 	}
 }
 
